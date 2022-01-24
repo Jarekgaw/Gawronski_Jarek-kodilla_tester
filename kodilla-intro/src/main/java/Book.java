0de29997cc5417cfdf2586d5d77d5111fbb166e1
Jarek Gawroński <jaroslaw.gawronski@gmail.com>
2022-01-24 16:14:07 +0100

    Book

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private static String author;
+    private static String title;
+    Book book = Book.of("Isaac Asimov", "The Galaxy");
+
+    public static Book of(String isaac_asimov, String the_galaxy) {
+
+        }
+
+    }
+}
