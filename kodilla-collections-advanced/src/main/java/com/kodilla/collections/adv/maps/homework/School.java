b1157e03179327ef25f5d6ca02e9c452a2cacef2
Jarek Gawroński <jaroslaw.gawronski@gmail.com>
2022-02-15 19:06:54 +0100

    Zadanie 5.2

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -0,0 +1,18 @@
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    private List<Integer> students = new ArrayList<>();
+
+    public School(int ... students) {
+        for (int student : students)
+            this.students.add(student);
+    }
+
+    @Override
+    public String toString() {
+        return "School" + students.toString();
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -0,0 +1,25 @@
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolDirectory {
+    public static void main(String[] args) {
+        Map<School, Principal> schoolDirector = new HashMap<>();
+        School firstLO = new School(200);
+        School tenLO = new School(230);
+        School nineLO = new School(150);
+
+        Principal kowalski = new Principal("Kowalski");
+        Principal adamska = new Principal("Adamska");
+        Principal tomaszewski = new Principal("Tomaszewski");
+
+        schoolDirector.put(firstLO, kowalski);
+        schoolDirector.put(tenLO, adamska);
+        schoolDirector.put(nineLO, tomaszewski);
+
+
+        for (Map.Entry<School, Principal> schoolEntry : schoolDirector.entrySet()) ;
+        System.out.println(schoolDirector.entrySet());
+    }
+}
