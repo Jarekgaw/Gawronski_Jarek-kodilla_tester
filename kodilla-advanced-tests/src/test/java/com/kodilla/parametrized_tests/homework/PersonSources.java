17179d95b274909ad91a6bb0f2bc30b1527374f5
Jarek Gawroński <jaroslaw.gawronski@gmail.com>
2022-03-13 21:25:51 +0100

    Zadanie 7.4

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
@@ -7,16 +7,9 @@ import java.util.stream.Stream;
 public class PersonSources {
     public static Stream<Arguments> personList() {
         return Stream.of(
-                Arguments.of("", new Person(1.76,65)),
-                Arguments.of("", new Person(1.55,76)),
-                Arguments.of("", new Person(1.85,103)),
-                Arguments.of("", new Person(1.73,54)),
-                Arguments.of("", new Person(1.65,120)),
-                Arguments.of("", new Person(1.75,74)),
-                Arguments.of("", new Person(1.95,100)),
-                Arguments.of("", new Person(1.55,54))
-
+                Arguments.of("18", new Person(1.76,65)),
+                Arguments.of("24,5", new Person(1.55,76)),
+                Arguments.of("27,8", new Person(1.85,103))
         );
-
     }
 }
