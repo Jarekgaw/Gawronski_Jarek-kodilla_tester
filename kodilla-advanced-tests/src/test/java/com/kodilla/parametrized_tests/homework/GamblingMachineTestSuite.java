d368f08ac6038d6a76e61149cf03706f13c1d3a5
Jarek Gawroński <jaroslaw.gawronski@gmail.com>
2022-03-29 21:13:36 +0200

    Zadanie 7.2

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -2,12 +2,11 @@ package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GamblingMachineTestSuite {
 
@@ -26,6 +25,7 @@ public class GamblingMachineTestSuite {
 
         assertThrows(InvalidNumbersException.class, () -> machines.howManyWins(number1));
     }
+
 }
 
 
