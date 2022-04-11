82380f7526d62d2c54e4f49738456e15cab96041
Jarek Gawroński <jaroslaw.gawronski@gmail.com>
2022-04-11 15:46:49 +0200

    Zadanie 9.4

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Cabrio.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Cabrio.java
@@ -2,21 +2,23 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 
 import java.time.LocalDateTime;
 
-public class Sedan implements Car {
+public class Cabrio implements Car{
+
     @Override
     public boolean hasHeadlightsTurnedOn() {
         LocalDateTime now = LocalDateTime.now();
         int time = now.getHour();
         if (20 > time && time > 6) {
             return false;
-        } else {
+        }
+        else {
             return true;
         }
+
     }
 
-        @Override
-        public String getCarType () {
-            return "Sedan";
-        }
+    @Override
+    public String getCarType() {
+        return "Cabrio";
     }
-
+}
--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/SUV.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/SUV.java
@@ -1,13 +1,22 @@
 package com.kodilla.spring.basic.spring_configuration.homework;
 
+import java.time.LocalDateTime;
+
 public class SUV implements Car{
     @Override
     public boolean hasHeadlightsTurnedOn() {
-        return false;
+        LocalDateTime now = LocalDateTime.now();
+        int time = now.getHour();
+        if (20 > time && time > 6) {
+            return false;
+        } else {
+            return true;
+        }
+
     }
 
     @Override
     public String getCarType() {
-        return null;
+        return "SUV";
     }
 }
